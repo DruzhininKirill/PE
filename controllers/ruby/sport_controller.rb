8f3ba502344366ea5334d96237be6657bf0b148b
DruzhininKirill <33481018+DruzhininKirill@users.noreply.github.com>
2019-02-12 21:19:27 +0400

    Remove useless folders

--- controllers/ruby/sport_controller.rb
+++
@@ -1,8 +0,0 @@
-class SportController < Rhoconnect::Controller::Base
-  register Rhoconnect::EndPoint
-
-  # register SYNC routes
-  register Rhoconnect::Handler::Sync
-
-  # add your custom routes here
-end
