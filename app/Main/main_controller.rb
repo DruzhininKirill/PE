eb0b37fc2cea118480924fecaa21f817bc5ee9e2
DruzhininKirill <33481018+DruzhininKirill@users.noreply.github.com>
2019-02-15 01:34:58 +0400

    implemented schedule

--- app/Main/main_controller.rb
+++ app/Main/main_controller.rb
@@ -1,20 +1,37 @@
 require 'rho/rhocontroller'
 require 'helpers/browser_helper'
 require 'net/http'
+require 'json'
 
 
 class MainController < Rho::RhoController
   include BrowserHelper
 
   def index
-    @contacts = Main.find(:all)
+    uri = URI.parse("http://calm-brook-51137.herokuapp.com/sport/schedule/")
+    user = {:user=> {
+        :login=> $session[:login],
+        :password=> $session[:password]
+    }
+    }
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.body = user.to_json
+    response = http.request(request)
+    puts "CASHA♥♥" + response.body
+    obj = eval(response.body.to_s)
+    data = obj[:sch].to_s
+    $session[:lessons] = obj[:sch]
+    @lessons = obj[:sch]
+
+    puts "!!!!array: "
     render :back => '/app'
   end
 
   def auth
     @login = @params['login']
     @password = @params['password']
-    uri = URI.parse("http://calm-brook-51137.herokuapp.com/sport/")
+    uri = URI.parse("http://calm-brook-51137.herokuapp.com/sport/login/")
 
     # header = {'Content-Type'=> 'text/json'}
 
@@ -31,22 +48,31 @@ class MainController < Rho::RhoController
     request.body = user.to_json
     response = http.request(request)
     puts "CASHA♥♥" + response.body
+    data = response.body.to_s
+    if data == '{"aut": "accept"}'
+      $session[:login] = @login
+      $session[:password] = @password
 
-    # Shortcut
-    # response = Net::HTTP.get_response(uri)
+      redirect :action => :index
+    else
+      Rho::Notification.showPopup({
+        :title => "Error",
+        :message => "No such user",
+        :buttons => ["Try again"]})
 
-    # Will print response.body
-    # Net::HTTP.get_print(uri)
-
-    # Full
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # response = http.request(Net::HTTP::Get.new(uri.request_uri))
-
-
-    Rho::Notification.showPopup({
-                                  :title => "Error",
-                                  :message => "send",
-                                  :buttons => ["Continue"]})
-    redirect :action => :index
+      redirect :model => '', :action=> :index
+    end
   end
+
 end
+
+
+# Shortcut
+# response = Net::HTTP.get_response(uri)
+
+# Will print response.body
+# Net::HTTP.get_print(uri)
+
+# Full
+# http = Net::HTTP.new(uri.host, uri.port)
+# response = http.request(Net::HTTP::Get.new(uri.request_uri))
