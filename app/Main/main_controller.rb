37cc15a780f860bdf9f89b404275f8d544f14ba7
DruzhininKirill <33481018+DruzhininKirill@users.noreply.github.com>
2019-02-15 03:00:14 +0400

    link issues fix and start developing admin

--- app/Main/main_controller.rb
+++ app/Main/main_controller.rb
@@ -28,6 +28,29 @@ class MainController < Rho::RhoController
     render :back => '/app'
   end
 
+  def adminpage
+    uri = URI.parse("http://calm-brook-51137.herokuapp.com/sport/schedule/")
+    user = {:user=> {
+        :login=> $session[:login],
+        :password=> $session[:password]
+    }
+    }
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.body = user.to_json
+    response = http.request(request)
+    puts "CASHA♥♥" + response.body
+    obj = eval(response.body.to_s)
+    data = obj[:sch].to_s
+    $session[:lessons] = obj[:sch]
+    @alllessons = obj[:sch]
+
+    puts "!!!!array: "
+    render :back => '/app'
+  end
+
+
+
   def auth
     @login = @params['login']
     @password = @params['password']
@@ -54,6 +77,13 @@ class MainController < Rho::RhoController
       $session[:password] = @password
 
       redirect :action => :index
+
+    elsif data == '{"aut": "accept_as_admin"}'
+      $session[:login] = @login
+      $session[:password] = @password
+      redirect :action => :adminpage
+
+      redirect :action => :index
     else
       Rho::Notification.showPopup({
         :title => "Error",
@@ -64,7 +94,18 @@ class MainController < Rho::RhoController
     end
   end
 
-end
+  def newuser
+    render :action => :newuser
+  end
+
+
+
+
+
+
+  end
+
+
 
 
 # Shortcut
